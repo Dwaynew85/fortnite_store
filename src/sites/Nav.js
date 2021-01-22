cdbb16931851bb078580027ef2bdaeed70a442ac
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-22 03:37:54 -0500

    added components for each site page

---
+++ src/sites/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <h3>Fortnite Store</h3>
+            <ul className="nav-links"> 
+                <li>Upcoming</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/sites/Upcoming.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Upcoming() {
+    return (
+        <div>
+            <h1>Upcoming Products</h1>
+        </div>
+    )
+}
+
+export default Upcoming
