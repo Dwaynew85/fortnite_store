e3c540314c59bf7eef10db2b77212c83a04dc4f5
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-22 16:17:08 -0500

    added item component to render list of upcoming items

--- src/sites/Upcoming.js
+++ src/sites/Upcoming.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import Item from '../components/Item'
 
 function Upcoming() {
+
+    useEffect(() => {
+        fetchItems()
+    },[])
+
+    const [items, setItems] = useState([])
+
+    const fetchItems = async () => {
+        const data = await fetch("https://fortnite-api.theapinetwork.com/upcoming/get")
+        const list = await data.json();
+        const items = list.data;
+        console.log(items)
+        setItems(items);
+    }
+
+    const itemList = items.map(item => (
+        <Item key={item.itemId} item={item}/>
+    ))
+
     return (
         <div>
-            <h1>Upcoming Products</h1>
+            {itemList}
         </div>
     )
 }
