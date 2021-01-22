aa102c79571d4498636a4ad11dd3dc65d29542fa
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-22 03:37:15 -0500

    added nav bar links and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Nav from './sites/Nav';
+import Home from './sites/Home';
+import Upcoming from './sites/Upcoming';
+import Cart from './sites/Cart';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path="/" component={Home}/>
+        <Route path="/upcoming" component={Upcoming}/>
+        <Route path="/cart" component={Cart}/>
+      </div>
+    </Router>
+    
   );
 }
 
