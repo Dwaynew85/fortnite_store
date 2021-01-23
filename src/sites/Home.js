b7a57864e1e762844a87a1a71d602c5099813a6a
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-23 02:04:02 -0500

    added main shop items on homepage

--- src/sites/Home.js
+++ src/sites/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import Item from '../components/Item'
 
-function Home() {
+function Upcoming() {
+
+    useEffect(() => {
+        fetchItems()
+    },[])
+
+    const [items, setShopItems] = useState([])
+
+    const fetchItems = async () => {
+        const data = await fetch("https://fortnite-api.theapinetwork.com/store/get")
+        const shoppingList = await data.json();
+        const shopItems = shoppingList.data;
+        setShopItems(shopItems);
+    }
+
+    const shoppingList = items.map(item => (
+        <Item key={item.itemId} item={item} cost={item.store.cost} />
+    ))
+
     return (
         <div>
-            <h1>Homepage</h1>
+            {shoppingList}
         </div>
     )
 }
 
-export default Home
+export default Upcoming
