1ef56206acc1e6399979552c3a3b1d7ab9665ea5
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-22 22:04:59 -0500

    added styles and item details

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Item.css';
 
 function Item(props) {
     const {item} = props.item
     console.log(item)
     return (
-        <div>
-            <h1>{item.name}</h1>
+        <div className="itemBox">
+            <fieldset>
+                <legend><h1>{item.name}</h1></legend>
+                <h2>{item.cost}</h2>
+                <h3>{item.description}</h3>
+                <img src={item.images.icon} alt={item.series} />
+            </fieldset>
         </div>
     )
 }
