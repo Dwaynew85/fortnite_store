2d6b2d904eb9e075d8370950aa699db7aea407fe
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-24 23:22:16 -0500

    added show page route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './sites/Nav';
 import Home from './sites/Home';
 import Upcoming from './sites/Upcoming';
 import Cart from './sites/Cart';
+import Show from './components/Show'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home}/>
           <Route path="/upcoming" component={Upcoming}/>
           <Route path="/cart" component={Cart}/>
+          <Route path="/item/:id" component={Show} />
         </Switch>
       </div>
     </Router>
