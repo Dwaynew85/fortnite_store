35d465919fb72e771b55bc2b04f93a3fb876689a
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-25 14:26:54 -0500

    added show page details

--- src/components/Show.js
+++ src/components/Show.js
@@ -12,10 +12,15 @@ function Show({ match }) {
         const data = await fetchItem.json();
         setItem(data.data.item)
     };
-
+console.log(item)
     return (
         <div>
-            <h1>Item</h1>
+            <img src={item.images.icon} alt={item.name} />
+            <h1>{item.name}</h1>
+            <p>{item.type}</p>
+            <h2>{item.description}</h2>
+            <h3>${item.cost}</h3>
+            <h4>Average Stars: {item.ratings.avgStars} TotalPoints: {item.ratings.totalPoints} Votes: {item.ratings.numberVotes}</h4>
         </div>
     )
 }
