d5b8d8ecdd12a53ecef4f206da74e33d377fb7ff
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-24 23:34:26 -0500

    added source to Link for Show page

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,14 +2,12 @@ import React from 'react';
 import '../styles/Item.css';
 import { Link } from 'react-router-dom';
 
-function Item(props, key) {
+function Item(props) {
     const {item} = props.item
-    console.log(item)
     return (
         <div className="itemBox">
             <fieldset>
-                {/* where is the item id? */}
-                <legend><Link to={`/item/`}><h1>{item.name}</h1></Link></legend> 
+                <legend><Link to={`/item/${props.item.itemId}`}><h1>{item.name}</h1></Link></legend> 
                 <h2>{props.item.store ? "$" + props.item.store.cost : item.cost}</h2>
                 <h3>{item.description ? item.description : item.name}</h3>
                 <img src={item.images.icon} alt={item.series} />
