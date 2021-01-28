932425f8eea8ad018840f89e0938b8a436617423
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-28 04:36:56 -0500

    added DevTools to store

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import manageCart from './reducers/manageCart';
 import {createStore} from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(manageCart);
+const store = createStore(manageCart, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
