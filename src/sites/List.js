d51e4e90d0f44855dd940bb5f7fc93830bd88fe4
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-28 03:40:00 -0500

    fixed warnings with fetchList function

--- src/sites/List.js
+++ src/sites/List.js
@@ -4,17 +4,17 @@ import Item from '../components/Item'
 function List({ match }) {
 
     useEffect(() => {
-        fetchList()
+        const fetchList = async () => {
+            const data = await fetch(`https://fortnite-api.theapinetwork.com${match.url}/get`)
+            const shoppingList = await data.json();
+            setShopLists(shoppingList.data);
+        }
+        fetchList();
     }, [match])
 
     const [lists, setShopLists] = useState([])
 
-    const fetchList = async () => {
-        const data = await fetch(`https://fortnite-api.theapinetwork.com${match.url}/get`)
-        const shoppingList = await data.json();
-        console.log(match.url)
-        setShopLists(shoppingList.data);
-    }
+    
 
     const shoppingList = lists.map(list => (
         <Item key={list.listId} item={list} />
