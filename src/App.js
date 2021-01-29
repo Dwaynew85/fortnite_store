808b4bc23169ac9476aa40ef58a55141fd765579
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-29 14:55:01 -0500

    added Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import SiteContainer from './containers/SiteContainer'
 
 
 function App() {
   return (
-    <SiteContainer />
+    <Router>
+      <SiteContainer />
+    </Router>
   );
 }
 
