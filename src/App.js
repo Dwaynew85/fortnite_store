a4125957fd3826da7ce9adf3e18324758dfb86ff
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-29 14:17:52 -0500

    modified to add SiteContainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import './App.css';
-import Nav from './sites/Nav';
-import List from './sites/List';
-import Cart from './sites/Cart';
-import Show from './components/Show'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import SiteContainer from './containers/SiteContainer'
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route exact path="/store" component={List}/>
-          <Route path="/upcoming" component={List}/>
-          <Route path="/cart" component={Cart}/>
-          <Route path="/item/:id" component={Show} />
-        </Switch>
-      </div>
-    </Router>
-    
+    <SiteContainer />
   );
 }
 
