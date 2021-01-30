82d6587986b4f22133e7db4370f6fdf687c11e59
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-30 05:57:24 -0500

    removed match and added useParams. added addToCart to child component

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,23 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import ItemDetail from './ItemDetail'
+import ItemDetail from './ItemDetail';
+import { useParams } from 'react-router-dom';
 
-function ShowItem({ match }) {
+function Show(props) {
+    let params = useParams();
+    const [item, setItem] = useState({});
+
     useEffect(() => {
         const fetchItem = async () => {
-            const fetchItem = await fetch(`https://fortnite-api.theapinetwork.com/item/get?id=${match.params.id}`)
+            const fetchItem = await fetch(`https://fortnite-api.theapinetwork.com/item/get?id=${params.id}`)
             const data = await fetchItem.json();
             setItem(data.data.item)
         };
         fetchItem();
     }, [])
 
-    const [item, setItem] = useState({});
     
     return (
         <div>
-            {item.name ? <ItemDetail item={item} /> : <h3>Loading...</h3> }
+            {item.name ? <ItemDetail item={item} addToCart={props.addToCart} /> : <h3>Loading...</h3> }
         </div>
     )
 }
 
-export default ShowItem
+export default Show
