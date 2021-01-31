772aaf8b439dcfe139dd380ada2b41a5e46c0a1f
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-01-31 03:10:46 -0500

    reducer adds to cart array

--- src/reducers/manageCart.js
+++ src/reducers/manageCart.js
@@ -3,7 +3,8 @@ const manageCart = (state = { cart: []}, action) => {
         
     case "ADD_TO_CART":
         console.log(action)
-        return { cart: state.cart.concat() };
+        return { ...state,
+            cart: [...state.cart, action.item] };
     case "REMOVE_FROM_CART":
         const cart = state.cart.filter(item => `${item.id}` !== action.id)
         return { cart }
