10532e6e61f3e35ddfa185b5daab6e6d7bbf6239
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-01 03:50:27 -0500

    added background key to item function and function to set background for each item

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,23 @@
 export const addIdToItem = (item, id) => {
     item.id = id;
+    item.background = setBackground(item);
     return item
+}
+
+export const setBackground = (item) => {
+    switch (item.rarity) {
+
+        case "common":
+            return "https://i.imgur.com/wE8JzTe.png"
+        case "uncommon":
+            return "https://i.imgur.com/BRxwgkS.png"
+        case "rare":
+            return "https://i.imgur.com/8ImiV3p.png"
+        case "epic":
+            return "https://i.imgur.com/4VGTgPv.png?2"
+        case "legendary":
+            return "https://i.imgur.com/41Thxnq.png"
+        default:
+            return "https://i.imgur.com/8Hiu0Lo.png"
+    }
 }
