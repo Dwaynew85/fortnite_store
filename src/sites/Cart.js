bf32cdf2b4628c5d57b15a7a7c4b6937b059ba40
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-02 01:48:37 -0500

    fixed error caused by totalCost function

--- src/sites/Cart.js
+++ src/sites/Cart.js
@@ -7,23 +7,30 @@ function Cart(props) {
         <CartLi key={list.id} item={list} delete={props.delete} />
     ))
 
-    const totalCost = props.cart.map(item => item.cost).reduce(function(acc, cur) {
+    const totalCost = props.cart.length === 0 ? 0 : props.cart.map(item => item.cost).reduce(function(acc, cur) {
         return acc + cur;
     });
+
+    //Need function to add cart items to state as order 
+
+    // Function required to send state to backend API
     
 
     return (
         <div>
             <form>
                 <label>First Name: </label>
-                <input type="text" name="firstName"></input>
+                <input type="firstName" name="firstName"/>
                 <label>Last Name: </label>
-                <input type="text" name="lastName"></input>
+                <input type="lastName" name="lastName"/>
+                <br/>
+                <label>Address: </label>
+                <input type="address" name="address" placeholder="Enter Full Address"/>
             </form>
             <ul>
                 {cartLists}
             </ul>
-            <h3>Total: ${totalCost}</h3> {/* not working */}
+            <h3>Total: ${totalCost}</h3> 
         </div>
     )
 }
