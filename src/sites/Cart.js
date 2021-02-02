fbb801a51045199b79247a9025203b04b2448ea2
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-02 04:30:33 -0500

    form component added

--- src/sites/Cart.js
+++ src/sites/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CartLi from '../components/CartLi'
+import CartInput from '../components/CartInput'
 
 function Cart(props) {
 
@@ -9,26 +10,11 @@ function Cart(props) {
 
     const totalCost = props.cart.length === 0 ? 0 : props.cart.map(item => item.cost).reduce(function(acc, cur) {
         return acc + cur;
-    });
-
-    //Need function to add cart items to state as order 
-
-    // Function required to send state to backend API
-    
+    });  
 
     return (
         <div>
-            <form>
-                <label>First Name: </label>
-                <input type="firstName" name="firstName"/>
-                <label>Last Name: </label>
-                <input type="lastName" name="lastName"/>
-                <br/>
-                <label>Address: </label>
-                <input type="address" name="address" placeholder="Enter Full Address"/>
-                <br/>
-                <label>-Card Info Would Go Here-</label>
-            </form>
+            <CartInput cart={props.cart}/>
             <ul>
                 {cartLists}
             </ul>
