1d0cc1679a5dea6800ac87cf412e47eacbb8d8dc
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-02 02:41:54 -0500

    added cart to props

--- src/components/Show.js
+++ src/components/Show.js
@@ -6,7 +6,7 @@ import { addIdToItem } from '../actions/index'
 function Show(props) {
     let params = useParams();
     const [item, setItem] = useState({});
-
+    
     useEffect(() => {
         const fetchItem = async () => {
             const fetchItem = await fetch(`https://fortnite-api.theapinetwork.com/item/get?id=${params.id}`)
@@ -18,7 +18,7 @@ function Show(props) {
     
     return (
         <div>
-            {item.name ? <ItemDetail item={addIdToItem(item, params.id)} addToCart={props.addToCart} /> : <h3>Loading...</h3> }
+            {item.name ? <ItemDetail item={addIdToItem(item, params.id)} addToCart={props.addToCart} cart={props.cart}/> : <h3>Loading...</h3> }
         </div>
     )
 }
