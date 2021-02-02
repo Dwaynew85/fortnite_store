83b26ccedfc362af92a6e246abac6ac900bfb9d2
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-02 00:52:06 -0500

    changed Link to make entire fieldset clickable

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,12 +6,14 @@ function Item(props) {
     const {item, cost} = props
     return (
         <div className="itemBox">
-            <fieldset className ="rarity" style={{backgroundImage: `url(${item.background})`}}>
-                <legend><Link to={`/item/${item.id}`}><h1>{item.name}</h1></Link></legend> 
-                <h2>${cost}</h2>
-                <h3>{item.description ? item.description : item.name}</h3>
-                <img src={item.images.icon} alt={item.costmeticId} />
-            </fieldset>
+            <Link to={`/item/${item.id}`}>
+                <fieldset className ="rarity" style={{backgroundImage: `url(${item.background})`}}>
+                    <legend><h1>{item.name}</h1></legend> 
+                    <img src={item.images.icon} alt={item.costmeticId} />
+                    <h2>${cost}</h2>
+                    <h3>{item.description ? item.description : item.name}</h3>
+                </fieldset>
+            </Link>
         </div>
     )
 }
