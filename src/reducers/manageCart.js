88c603d3979498a1983dd3df045a52f34c498fe6
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-02 02:11:48 -0500

    fixed DELETE_FROM_CART to actually deletefrom the cart

--- src/reducers/manageCart.js
+++ src/reducers/manageCart.js
@@ -2,11 +2,10 @@ const manageCart = (state = { cart: []}, action) => {
     switch (action.type) {
         
     case "ADD_TO_CART":
-        console.log(action)
         return { ...state,
             cart: [...state.cart, action.item] };
-    case "REMOVE_FROM_CART":
-        const cart = state.cart.filter(item => `${item.id}` !== action.id)
+    case "DELETE_FROM_CART":
+        const cart = state.cart.filter(item => `${item.id}` !== action.item.id)
         return { cart }
 
     default:
