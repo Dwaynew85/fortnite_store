4098ed8f4642b54a477e3e564f640c75eb09aa5d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-03 12:45:30 -0500

    refactored with deconstructors

--- src/components/Show.js
+++ src/components/Show.js
@@ -3,7 +3,7 @@ import ItemDetail from './ItemDetail';
 import { useParams } from 'react-router-dom';
 import { addIdToItem } from '../actions/index'
 
-function Show(props) {
+function Show({ addToCart, cart }) {
     let params = useParams();
     const [item, setItem] = useState({});
     
@@ -14,11 +14,11 @@ function Show(props) {
             setItem(data.data.item)
         };
         fetchItem();
-    }, [])
+    }, [params.id])
     
     return (
         <div>
-            {item.name ? <ItemDetail item={addIdToItem(item, params.id)} addToCart={props.addToCart} cart={props.cart}/> : <h3>Loading...</h3> }
+            {item.name ? <ItemDetail item={addIdToItem(item, params.id)} addToCart={addToCart} cart={cart}/> : <h3>Loading...</h3> }
         </div>
     )
 }
