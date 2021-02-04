55a862754fe4451e0b47af4c1b0956e8658a091e
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-04 05:23:25 -0500

    fixed error in store

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './reducers/manageCartAndOrders'
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import  thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 
 ReactDOM.render(
