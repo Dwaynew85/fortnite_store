e3755788971d75832405a99f37bc21d714a9ad3d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-04 14:26:37 -0500

    consolidated reducers to removed nested store keys

--- src/reducers/manageCartAndOrders.js
+++ src/reducers/manageCartAndOrders.js
@@ -1,38 +1,32 @@
-import { combineReducers } from 'redux';
-
-const cartReducer = (state = { cart: []}, action) => {    
+const manageCartAndOrdersReducer = (state = { cart: [], orders: [], requesting: false}, action) => {
     switch (action.type) {
+
+        case "ADD_TO_CART":
+            return { 
+                ...state,
+                cart: [
+                    ...state.cart, 
+                    action.item
+                ]};
+
+        case "DELETE_FROM_CART":
+            const cart = state.cart.filter(item => `${item.id}` !== action.item.id)
+            return { cart }
         
-    case "ADD_TO_CART":
-        return { 
-            ...state,
-            cart: [
-                ...state.cart, 
-                action.item
-            ]};
-    case "DELETE_FROM_CART":
-        const cart = state.cart.filter(item => `${item.id}` !== action.item.id)
-        return { cart }
-
-    default:
-        return state
-    }
-}
-
-const ordersReducer = (state = { orders: [], requesting: false }, action) => {
-    switch (action.type) {
-
         case 'START_ADDING_ORDERS_REQUEST':
             return { 
                 ...state,
                 orders: [...state.orders],
                 requesting: true
         }
-
+        
         case 'ADD_ORDERS':
             return {
                 ...state,
-                orders: action.orders,
+                orders: [
+                    ...state.orders,
+                    action.orders
+                ],
                 requesting: false
             }
 
@@ -41,9 +35,4 @@ const ordersReducer = (state = { orders: [], requesting: false }, action) => {
     }
 }
 
-const rootReducer = combineReducers({
-    cart: cartReducer,
-    orders: ordersReducer
-});
-
-export default rootReducer;
+export default manageCartAndOrdersReducer;
