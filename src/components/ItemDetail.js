7f259abd1c087f97c47084b2cd72c0927950871b
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-04 02:02:31 -0500

    simplified deconstructor and added console log containing media details for specific items

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,19 +1,18 @@
 import React from 'react'
 
-function ItemDetail(props) {
-    const { item, addToCart } = props
-    
+function ItemDetail({ item, cart, addToCart }) {
+    console.log(item.media.length >=1 ? item.media[0].src : "no media")
     return (
-        <div className ="rarity" style={{backgroundImage: `url(${item.background})`}}>
+        <fieldset className ="rarity" style={{backgroundImage: `url(${item.background})`}}>
+            <legend><h1>{item.name}</h1></legend>
             <img src={item.images.background} alt={item.name}/>
-            <h1>{item.name}</h1>
-            <p>{item.type}</p>
             <h2>{item.description}</h2>
+            <p>{item.type}</p>
             <h3>${item.cost}</h3>
             <h4>Average Stars: {item.ratings.avgStars} TotalPoints: {item.ratings.totalPoints} Votes: {item.ratings.numberVotes}</h4>
             {/* add video tag {item.media....} */}
-            {item.cost === 0 || props.cart.includes(item) ? "" : <button onClick={(e) => addToCart(item)}>Add To Cart</button>}
-        </div>
+            {item.cost === 0 || cart.includes(item) ? "" : <button onClick={(e) => addToCart(item)}>Add To Cart</button>}
+        </fieldset>
     )
 }
 
