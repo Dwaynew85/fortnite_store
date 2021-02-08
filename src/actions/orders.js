75cb22b58d7afb8383d02eca7da68393fd901553
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-08 02:23:37 -0500

    added fetchOrders function to fetch orders list from backend

--- src/actions/orders.js
+++ src/actions/orders.js
@@ -1,15 +1,31 @@
 // function to send POST request to add order to backend. to be run when order is submitted.
-export const submitOrder = (order) => {
-    fetch ('http://localhost:3001/orders', {
+export const submitOrder = async (order) => {
+    const settings = {
         method: 'POST',
-        headers: {"Content-type": "application/json;charset=UTF-8"},
+        headers: {
+            Accept: 'application/json',
+            "Content-type": "application/json;charset=UTF-8"
+        },
         body: JSON.stringify(order)
-    })
-    .then(response => response.json())
-    .then(json => console.log(json)/* What are we doing with the data we get back? */)
-    .catch(err => console('Request Failed', err));
+    };
+    try {
+        const fetchOrder = await fetch('http://localhost:3001/orders', settings);
+        const data = await fetchOrder.json();
+        console.log(data);
+    } catch (e) {
+        return e;
+    }
+    
 }
 
 // function to send GET request for orders in backend. to be run when app mounts
+export const fetchOrders = () => {
+    return async (dispatch) => {
+        dispatch({ type: 'START_ADDING_ORDERS_REQUEST'});        
+        fetch('http://localhost:3001/orders')
+            .then(response => response.json())
+            .then(orders => dispatch({ type: 'ADD_ORDERS', orders }));      
+    };
+}
 
 // function to clear state after form is submitted 
