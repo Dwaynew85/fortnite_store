f70ed61c4b697c996e75e522efb4ce0c747f7cc1
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-08 15:03:36 -0500

    added order details to OrderLi

---
+++ src/components/OrderLi.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function OrderLi({user, items, total}) {
+    
+    const itemLi = items.map(item => {
+        return (
+            <li>
+                <img src={item.icon} alt={item.name}></img>
+                <h6>{item.name}</h6>
+                <h6>{item.cost}</h6>
+            </li>
+        )
+    })
+    
+    return (
+        <div>
+            <h3>Name: {user.first_name} {user.last_name}</h3>
+            <h4>Email: {user.email}</h4>
+            <h5>Shipped to: {user.address}</h5>
+            <ul>
+                {itemLi}
+            </ul>
+            <h4>Order total: {total}</h4>
+        </div>
+    )
+}
