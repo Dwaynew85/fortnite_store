024d69d82890d9255feacde36177a71f9c4c53dc
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-08 02:43:30 -0500

    added OrderHistory site to handle list of orders from state

---
+++ src/sites/OrderHistory.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react';
+import OrderLi from '../components/OrderLi'
+
+export default function OrderHistory({fetchOrders, orders}) {
+    
+    useEffect(() => {
+        fetchOrders()
+    }, [fetchOrders])
+
+    const ordersList = orders.map(order => (
+        <li><OrderLi key={order.id} user={order.user} items={order.items} total={order.total} /></li>
+    ))
+
+    return (
+        <div>
+            {ordersList}
+        </div>
+    )
+}
