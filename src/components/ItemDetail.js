9d5cbc470f8d4c5ed52e8e9d1a3fce797830b765
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-09 02:59:46 -0500

    added video tag

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 function ItemDetail({ item, cart, addToCart }) {
-    console.log(item.media.length >=1 ? item.media[0].src : "no media")
     return (
         <fieldset className ="rarity" style={{backgroundImage: `url(${item.background})`}}>
+            {item.media.length >=1 ? <video width="20%" height="20%" controls autoplay loop><source src={item.media[0].src} type="video/mp4" autoplay/></video> : ""}
             <legend><h1>{item.name}</h1></legend>
             <img src={item.images.background} alt={item.name}/>
             <h2>{item.description}</h2>
             <p>{item.type}</p>
             <h3>${item.cost}</h3>
-            <h4>Average Stars: {item.ratings.avgStars} TotalPoints: {item.ratings.totalPoints} Votes: {item.ratings.numberVotes}</h4>
-            {/* add video tag {item.media....} */}
+            <h4>Average Stars: {item.ratings.avgStars} TotalPoints: {item.ratings.totalPoints} Votes: {item.ratings.numberVotes}</h4>            
             {item.cost === 0 || cart.includes(item) ? "" : <button onClick={(e) => addToCart(item)}>Add To Cart</button>}
         </fieldset>
     )
