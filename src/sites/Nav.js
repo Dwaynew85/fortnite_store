f4f6dea1f53e70c17cd777450856bad96fe7b18c
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-13 05:08:28 -0500

    added tags for nav styling

--- src/sites/Nav.js
+++ src/sites/Nav.js
@@ -6,17 +6,17 @@ export default function Nav({cart}) {
     return (
         <nav id="nav">
             <Link to="/store">
-                <h1 className="title-icon">Fortnite Store</h1>
+                <h1 className="title-icon" id="link-left">Fortnite Store</h1>
             </Link>
             <ul className="nav-links"> 
                 <Link to="/upcoming">
-                    <li className="title-icon">Upcoming</li>
+                    <li className="title-icon" id="link-right">Upcoming</li>
                 </Link>
                 <Link to="/cart">
-                    {cart.length === 0 ? '' : <li className="title-icon">Cart({cart.length})</li> }
+                    {cart.length === 0 ? '' : <li className="title-icon" id="link-right">Cart({cart.length})</li> }
                 </Link>
                 <Link to="/history">
-                    <li className="title-icon">Order History</li>
+                    <li className="title-icon" id="link-right">Order History</li>
                 </Link>
             </ul>
         </nav>
