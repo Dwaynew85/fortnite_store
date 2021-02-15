74c74c29f471bcb43995339e05a8137f9a34d55d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-15 03:57:52 -0500

    added tags for styling

--- src/components/CartLi.js
+++ src/components/CartLi.js
@@ -3,10 +3,12 @@ import React from 'react'
 export default function CartLi({item, deleteFromCart}) {
     return (
         <li className="cart-items">
-            <button className="" onClick={(e) => deleteFromCart(item)}>X</button>       
-            <img src={item.images.icon} alt={item.costmeticId} className ="rarity" style={{backgroundImage: `url(${item.background})`}} />
-            <h2>{item.name}</h2> 
-            <h3>${item.cost}</h3>    
+            <button className="delete" onClick={(e) => deleteFromCart(item)}>X</button>       
+            <img src={item.images.icon} alt={item.costmeticId} className ="rarity cart-icon" style={{backgroundImage: `url(${item.background})`}} />
+            <div className="cart-text">
+                <h2>{item.name}</h2> 
+                <h3>${item.cost}</h3>  
+            </div>  
         </li>
     )
 }
