6363663657143ab5b114e9d486e0f29b80c3a746
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-17 04:44:31 -0500

    styling tags added and UI/UX improvements

--- src/components/OrderItems.js
+++ src/components/OrderItems.js
@@ -4,7 +4,7 @@ export default function OrderItems({user, items, total, created}) {
     
     const itemLi = items.map(item => {
         return (
-            <li>
+            <li className="list-left">
                 <img className="item-icon" src={item.icon} alt={item.name}></img>
                 <h6>{item.name}</h6>
                 <h6>{item.cost}</h6>
@@ -13,15 +13,15 @@ export default function OrderItems({user, items, total, created}) {
     })
     
     return (
-        <div>
+        <div className="history-item">
             <h6>Order placed: {created}</h6>
-            <h3>Name: {user.first_name} {user.last_name}</h3>
-            <h4>Email: {user.email}</h4>
-            <h5>Shipped to: {user.address}</h5>
-            <ul>
+            <h2 className="history-item-child">Name: {user.first_name} {user.last_name}</h2>
+            <h3 className="history-item-child">Email: {user.email}</h3>
+            <h4 className="history-item-child">Shipped to: {user.address}</h4>
+            <ul className="history-item-child">
                 {itemLi}
             </ul>
-            <h4>Order total: {total}</h4>
+            <h4 className="history-item-child">Order total: {total}</h4>
         </div>
     )
 }
--- src/sites/OrderHistory.js
+++ src/sites/OrderHistory.js
@@ -9,7 +9,7 @@ export default function OrderHistory({fetchOrders, orders}) {
     }, [fetchOrders])
 
     const ordersList = orders.map(order => (
-        <li><OrderItems key={order.id} user={order.user} items={order.items} total={order.total} created={order.created_at} /></li>
+        <OrderItems key={order.id} user={order.user} items={order.items} total={order.total} created={order.created_at}/>
     ))
 
     return (
